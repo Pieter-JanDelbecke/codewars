ca4a9cde69b9c7c1160d6b085fb752a28d358ed0
Pieter-Jan Delbecke <pieterjan.delbecke@lexicondigital.com.au>
2022-06-19 11:08:01 +1000

    get the middle character

--- src/main/kotlin/Codewars.kt
+++ src/main/kotlin/Codewars.kt
@@ -1,3 +1,12 @@
 fun main(args:Array<String>){
-    println("Hello World")
+    println(getMiddle("test"))
+    println(getMiddle("middle"))
+    println(getMiddle("A"))
+    println(getMiddle("dre"))
+}
+
+fun getMiddle(word:String):String{
+    val length: Int = word.length
+    val half: Int = length/2
+    return if(length%2 == 0)word.substring(half-1, half+1) else word[half].toString()
 }
